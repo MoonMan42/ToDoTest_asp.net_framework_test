84a178f7dbc2068efbb8c330242cdaa728559bb9
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-07-24 19:25:55 -0400

    Add project files.

---
+++ ToDoTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ToDoTest.Startup))]
+namespace ToDoTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
