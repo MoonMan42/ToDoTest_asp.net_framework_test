3598b6adadff72b4d14f2736b87633f8e61d52d2
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-07-24 19:36:13 -0400

    Setup Todo model and moved DbContext to its own file.

---
+++ ToDoTest/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace ToDoTest.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<ToDoModel> ToDoList { get; set; } // connection to db
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ ToDoTest/Models/ToDoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoTest.Models
+{
+    public class ToDoModel
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        [Display(Name = "What do I need to do?")]
+        public string ToDo { get; set; }
+
+        [Required]
+        [Display(Name = "When does it need to be done?")]
+        public DateTime DueDate { get; set; }
+    }
+}
