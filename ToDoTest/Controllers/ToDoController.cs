870eca92e00630ba01f14340103414608ce8123a
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-07-24 19:52:40 -0400

    Added the index page for the ToDoController

---
+++ ToDoTest/Controllers/ToDoController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ToDoTest.Models;
+
+namespace ToDoTest.Controllers
+{
+    public class ToDoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ToDoController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: ToDo
+        public ActionResult Index()
+        {
+            var todoList = _context.ToDoList.ToList();
+
+            return View(todoList);
+        }
+    }
+}
