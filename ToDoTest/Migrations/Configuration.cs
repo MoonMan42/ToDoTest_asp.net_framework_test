c319ee9a3b1bfcfb47684f5017ab3e1dddfb0f85
MoonMan42 <34987599+MoonMan42@users.noreply.github.com>
2020-07-24 19:40:56 -0400

    added migration to the database Enable-migrations Add-migration _initial update-database

---
+++ ToDoTest/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ToDoTest.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ToDoTest.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ToDoTest.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
